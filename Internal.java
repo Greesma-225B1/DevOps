c073591389ab84181b4a0633fd13e10eda2205b6
GREESMA <147132301+Greesma-225B1@users.noreply.github.com>
2024-12-13 10:35:57 +0530

    Create Internal.java

---
+++ Internal.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Internal{
+  public static void main(String[] args){
+    int a,b;
+    Scanner sc = new Scanner(System.in);
+    a = sc.nextInt();
+    b = sc.nextInt();
+    if(a<b){
+      System.out.println(a+b);
+    }
+    else{
+      System.out.println(a-b);
+    }
